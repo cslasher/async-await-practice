584e2009724dddb7cedda01ff2587435c98b0038
Edwin Liu <cslasher@gmail.com>
2017-07-25 19:02:26 +0800

    Convert to asycn/await and add error handling

--- currency-convert.js
+++ currency-convert.js
@@ -3,16 +3,29 @@
 
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-	return axios.get(`http://api.fixer.io/latest?base=${from}`).then((res) => {
-		return res.data.rates[to];
-	});
+const getExchangeRate = async (from, to) => {
+	try {
+		const res = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+		const rate = res.data.rates[to];
+		if (rate) {
+			return rate;
+		} else {
+			throw new Error;
+		}
+	} catch (e) {
+		throw new Error(`Unable to get currency rate of ${from} or ${to}.`);
+	}
+
 }
 
-const getCountries = (currencyCode) => {
-	return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((res) => {
+const getCountries = async (currencyCode) => {
+	try {
+		const res = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 		return res.data.map((country) => country.name);
-	})
+	} catch (e) {
+		throw new Error(`Unable to get country with code ${currencyCode}.`);
+	}
+
 }
 
 // const convertCurrency = (from, to, amount) => {
@@ -33,10 +46,11 @@ const convertCurrency = async (from, to, amount) => {
 	const countries = await getCountries(to);
 
 	return `${amount} ${from} is worth ${exchangeAmount.toFixed(2)} ${to}. ${to} can be used at the following countries: ${countries.join(', ')}.`;
-
 }
 
 convertCurrency('USD', 'EUR', 100).then((res) => {
 	console.log(res);
+}).catch((e) => {
+	console.log(e.message);
 })
 
